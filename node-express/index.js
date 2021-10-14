dea0a6d544660ba12df77895112e98589e3d4238
xu <270197253@qq.com>
2021-10-14 17:00:47 +0800

    express generator

--- node-express/index.js
+++ node-express/index.js
@@ -1,23 +1,26 @@
-const express = require('express'),
-      http = require('http');
+const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+
 app.use(morgan('dev'));
-app.use(bodyParser.json());
+
+const leaderRouter = require('./routes/leaderRouter');
+app.use('/leaders', leaderRouter.router);
+
+const dishRouter = require('./routes/dishRouter');
+app.use('/dishes', dishRouter.router);
+
+const promotionRouter = require('./routes/promotionRouter');
+app.use('/promotions', promotionRouter.router);
 
 
-app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, hostname, function () {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
